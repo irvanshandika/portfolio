87e54daf871922fa2ebd4a0f0ea77626d56cc3c2
Irvan Shandika <shandikamuhammadirvan@gmail.com>
2025-03-27 17:17:04 +0700

    Update dependencies and adjust Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,5 @@ export default defineConfig({
   ],
 
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
 });
